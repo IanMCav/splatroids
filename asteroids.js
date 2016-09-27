e52a8319348a3c52289dddeaf4318fac414f1dd9
The-Darkling <flameian@mac.com>
2016-09-26 20:51:57 -0400

    created asteroids constructor, completing custom vector class.

---
+++ asteroids.js
@@ -0,0 +1,5 @@
+"use strict"
+function asteroid(position) {
+      this.
+    
+};
---
+++ vector.js
@@ -0,0 +1,40 @@
+// 2D vector class object for further use in js.
+// Assuming initial point is the origin
+// for use only with translations
+function Vector(x, y) {
+    "use strict";
+    //standard x and y coordinates.
+    this.x = x;
+    this.y = y;
+    
+    //Returns magnitude
+    this.magnitude = function() {
+        return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+    };
+    
+    //Returns direction (in rads)
+    this.direction = function() {
+        return Math.atan((this.x / this.y));
+    };
+    
+    //Add two vectors
+    this.addVec = function(otherVec) {
+        return new Vector(this.x + otherVec.x, this.y + otherVec.y);
+    };
+    
+    //Subtract two vectors
+    this.subVec = function(otherVec) {
+        return new Vector(this.x - otherVec.x, this.y + otherVec.y);
+    };
+    
+    //Multiply vector and scalar
+    this.multScalar = function(multInt) {
+        return new Vector(this.x * multInt, this.y * multInt);
+    };
+    
+    //dot product
+    this.dot = function(dotVec) {
+        return ((this.x * dotVec.x) + (this.y * dotVec.y));
+    };
+
+}
