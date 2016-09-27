177403fd22a4e705c60b9567e3b5e19e07aa9602
The-Darkling <flameian@mac.com>
2016-09-26 21:52:16 -0400

    test asteroid drawing, first bug- javascript inheritance is incorrect!

--- js/asteroids.js
+++ js/asteroids.js
@@ -4,5 +4,34 @@ function asteroid(newX, newY) {
     this.velocity = new Vector(0, 0);
     this.direction = new Vector(0, 0);
     
+    this.circRad = 20;
     
+    this.numTris = (Math.random() * 16) + 40
+    
+    var canvas = document.querySelector('#can');
+    var ctx = canvas.getContext("2d");
+    
+    this.draw = function() {
+        ctx.fillStyle = "black";
+        
+        ctx.beginPath();
+        for(var i = 0; i < this.numTris; i++)
+        {
+            ctx.save();
+            ctx.translate(this.position.x, this.position.y);
+            ctx.moveTo(0, 0);
+            var angle = Math.random() * 6.28319;
+            ctx.lineTo(Math.cos(angle) * Math.random() * this.circRad,
+                      Math.sin(angle) * Math.random() * this.circRad);
+
+            ctx.lineTo(Math.random()*10, Math.random()*10);
+            ctx.lineTo(Math.random()*10, Math.random()*10);
+            ctx.restore();
+        }
+        
+        ctx.arc(this.x,this.y, this.circRad, 0, 2*Math.PI);
+        ctx.fill();
+        
+        
+    }
 }
